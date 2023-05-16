f2554c6c511e02c8a697a0c87397cef56721b9ad
KillerxDBr <antonioroberto1407@gmail.com>
2023-05-15 22:50:01 -0400

    Color Tint + fix typos

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import struct
 import argparse
 
 # Constants
-COLOR    = 0x48
-NORMAL   = 0x54
-SPEC     = 0x60
-ENVPARAM = 0x6C
-TECHSET  = 0x34
+COLOR     = 0x48
+NORMAL    = 0x54
+SPEC      = 0x60
+ENVPARAM  = 0x6C
+COLORTINT = 0x7C
+TECHSET   = 0x34
 TECHSET_FLAGS = {
     'COLOR'     : "c0",
     'NORMAL'    : "n0",
@@ -28,19 +29,19 @@ class MTL:
         # Based on CoD 4 Assets
 
         # Offsets
-        mtl_name_offset = struct.unpack('i', mtl_file[0:4])[0]
+        mtlName_offset  = struct.unpack('i', mtl_file[0:4])[0]
         techset_offset  = struct.unpack('i', mtl_file[TECHSET:TECHSET+4])[0]
         color_offset    = struct.unpack('i', mtl_file[COLOR:COLOR+4])[0]
         normal_offset   = struct.unpack('i', mtl_file[NORMAL:NORMAL+4])[0]
-        spec_offser     = struct.unpack('i', mtl_file[SPEC:SPEC+4])[0]
+        spec_offset     = struct.unpack('i', mtl_file[SPEC:SPEC+4])[0]
 
         # Texture Names
-        self.mtl_name    = self.getMtlString(mtl_file, mtl_name_offset)
+        self.mtlName     = self.getMtlString(mtl_file, mtlName_offset)
         self.techset     = self.getMtlString(mtl_file, techset_offset)
         self.techsetArgs = self.getTechsetArgs()
         self.color       = self.getMtlString(mtl_file, color_offset)
         self.normal      = self.getMtlString(mtl_file, normal_offset)
-        self.raw_spec    = self.getMtlString(mtl_file, spec_offser)
+        self.raw_spec    = self.getMtlString(mtl_file, spec_offset)
 
         # Names for conversion
         self.spec       = '_'.join(self.raw_spec.split('&')[0].strip('~').split('_')[:-1]) + SPEC_SUFFIX
@@ -55,16 +56,18 @@ class MTL:
                                                 bytes([int(x) for x in mtl_file[ENVPARAM:ENVPARAM+16]]))
         
         self.envMapMin, self.envMapMax = envMapMin/4, envMapMax/4
+        self.colorTint = struct.unpack('4f', bytes([int(x) for x in mtl_file[COLORTINT:COLORTINT+16]]))
 
         # todo
         # Check if WaW envMapParms is different
     
     def __str__(self):
         return (f'\n------------------\n'
-                f'Material: {self.mtl_name}\n'
+                f'Material: {self.mtlName}\n'
                 f'Techset: {self.techset}\n'
                 f'Techset Args: {[flag for flag, value in self.techsetArgs.items() if value]}\n'
                 f'Color Map: {self.color}\n'
+                f'Color Tint: R: {self.colorTint[0]:.1f} G: {self.colorTint[1]:.1f} B: {self.colorTint[2]:.1f} A: {self.colorTint[3]:.1f}\n'
                 f'Normal Map: {self.normal}\n'
                 f'Raw Specular Map: {self.raw_spec}\n'
                 f'Specular Map: {self.spec}\n'
@@ -85,11 +88,12 @@ class MTL:
         for flag, value in TECHSET_FLAGS.items():
             tsarg.update({flag:(value in self.techset)})
         return tsarg
-
+    
+    # Add Color Tint parameter to GDT ----------------
     def toGDT(self):
-        path = f'texture_assets\\\\CoD4\\\\{self.mtl_name}\\\\'
+        path = f'texture_assets\\\\CoD4\\\\{self.mtlName}\\\\'
         string = (
-            f'\t"{self.mtl_name}" ( "material.gdf" )\n'
+            f'\t"{self.mtlName}" ( "material.gdf" )\n'
             f'\t{{\n'
             f'\t\t"template" "material.template"\n'
 		    f'\t\t"materialType" "model phong"\n'
@@ -146,12 +150,12 @@ for mtlpath in paths:
     with open(f'materials/{mtlpath}','rb') as f:
         mtl = f.read()
     mtl_classe.append(MTL(mtl))
-    print(f'\n{mtl_classe[-1].mtl_name}\n')
-    print(f'Float\n{bConvert(mtl,"f")}\n'
-          f'{"-"*50}'
-          f'\nInt\n{bConvert(mtl,"i")}')
+    # print(f'\n{mtl_classe[-1].mtlName}\n'
+    #       f'Float\n{bConvert(mtl,"f")}\n'
+    #       f'{"-"*50}'
+    #       f'\nInt\n{bConvert(mtl,"i")}')
 
-# for mtl in mtl_classe:
-#     print(mtl.toGDT())
+for mtl in mtl_classe:
+    print(mtl.__str__())
 
-print()
+# print(mtl_classe[0].__str__())
