0f9a56ed26b06b6c711fb5ba71521c0e525460ba
KillerxDBr <antonioroberto1407@gmail.com>
2023-05-28 00:34:15 -0400

    gitignore updates and file deletion, echo=True in main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_NAME = 'teste.db'
 
-engine = create_engine(f"sqlite:///{BASE_DIR}/{DB_NAME}", echo=False)
+engine = create_engine(f"sqlite:///{BASE_DIR}/{DB_NAME}", echo=True)
 
 session = scoped_session(
     sessionmaker(
